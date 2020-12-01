0282b50b52962f2092dbf66bb7610e970d2453c8
Nicola Corti <corti.nico@gmail.com>
2020-12-01 22:03:59 +0100

    AOC - Day 1

---
+++ src/main/java/Exercise1-part2.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val input = object {}.javaClass.getResource("input-1.txt").readText().split("\n")
+
+    val numberSet = input.map { it.toInt() }.toSet()
+
+    numberSet.forEachIndexed { index1, num1 ->
+        numberSet.forEachIndexed { index2, num2 ->
+            if (index1 != index2 && 2020 - num1 - num2 in numberSet) {
+                println(num1 * num2 * (2020 - num1 - num2))
+            }
+        }
+    }
+}
---
+++ src/main/java/Exercise1.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val input = object {}.javaClass.getResource("input-1.txt").readText().split("\n")
+
+    val numberSet = input.toSet().map { it.toInt() }
+    numberSet
+        .first { (2020 - it) in numberSet }
+        .let { it * (2020 - it) }
+        .also(::println)
+}
