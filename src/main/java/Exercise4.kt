5007b13971742688044ab0cdaa083d38dc2896fa
Nicola Corti <corti.nico@gmail.com>
2020-12-04 22:40:03 +0100

    AOC - Day 4

---
+++ src/main/java/Exercise4.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val input = object {}.javaClass.getResource("input-4.txt").readText()
+
+    input.splitToSequence("\n\n")
+        .filter {
+            it.split(' ', '\n')
+                .map { item -> item.substring(0, item.indexOf(':')) }
+                .containsAll(listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"))
+        }
+        .count()
+        .also(::println)
+}
