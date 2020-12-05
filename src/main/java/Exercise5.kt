2762323b555bbd2b3eff4b2e5dfbaaec69840fa7
Nicola Corti <corti.nico@gmail.com>
2020-12-05 22:37:18 +0100

    AOC - Day 5

---
+++ src/main/java/Exercise5.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val input = object {}.javaClass.getResource("input-5.txt").readText().split('\n')
+
+    input.map {
+        val row = findBinary(128, it.substring(0, 7))
+        val column = findBinary(8, it.substring(7))
+        (row * 8) + column
+    }.maxOrNull().also(::println)
+}
+
+fun findBinary(max: Int, key: String): Int {
+    var l = 0
+    var r = max
+    var idx = 0
+    while (idx < key.length) {
+        when (key[idx]) {
+            'F', 'L' -> r = (l+r)/2
+            'B', 'R' -> l = (l+r)/2
+        }
+        idx++
+    }
+    return l
+}
+
