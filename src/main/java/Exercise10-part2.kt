2b91c47bc5a17642edc49a14c6061e152ccaab37
Nicola Corti <corti.nico@gmail.com>
2020-12-12 22:51:20 +0100

    AOC - Day 10

---
+++ src/main/java/Exercise10-part2.kt
@@ -0,0 +1,28 @@
+fun main() {
+    object {}.javaClass.getResource("input-10.txt").readText().split("\n")
+        .map(String::toLong)
+        .toMutableList().apply {
+            add(0)
+            sort()
+            add(last() + 3)
+            countCombinations(this, size - 1).also(::println)
+        }
+}
+
+val map = mutableMapOf<Int, Long>()
+
+fun countCombinations(list: List<Long>, index: Int): Long {
+    if (index == 0) return 1
+    if (index in map) return map[index]!!
+    var count = 0L
+    (1..3).forEach { step ->
+        if (index >= step && list[index - step] in list[index] - 3..list[index] - step) {
+            count += countCombinations(list, index - step)
+        }
+    }
+    map[index] = count
+    return count
+}
+
+
+
---
+++ src/main/java/Exercise10.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val input = object {}.javaClass.getResource("input-10.txt").readText().split("\n")
+        .map(String::toLong)
+        .toMutableList().apply {
+            add(0)
+            sort()
+            add(last() + 3)
+        }
+    var count1 = 0
+    var count3 = 0
+
+    for (i in 0 until input.size - 1) {
+        println(input[i])
+        when (input[i]) {
+            input[i + 1] - 1 -> {
+                count1++
+            }
+            input[i + 1] - 3 -> {
+                count3++
+            }
+        }
+    }
+    println(count1 * count3)
+}
+
+
