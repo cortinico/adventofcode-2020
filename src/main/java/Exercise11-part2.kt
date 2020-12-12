71291abf9f11d930109547e6e2869a172ffecf70
Nicola Corti <corti.nico@gmail.com>
2020-12-13 00:14:16 +0100

    AOC - Day 11

---
+++ src/main/java/Exercise11-part2.kt
@@ -0,0 +1,76 @@
+fun main() {
+    val input: List<CharArray> = object {}.javaClass.getResource("input-11.txt").readText()
+        .split("\n")
+        .map(String::toCharArray)
+
+    var array1 = Array(input.size) { i ->
+        CharArray(input[i].size) { j ->
+            input[i][j]
+        }
+    }
+    var array2 = Array(input.size) { i -> CharArray(input[i].size) { ' ' } }
+    var occupiedPlaces: Int
+
+    do {
+        var countedChanges = 0
+        occupiedPlaces = 0
+        for (i in array1.indices) {
+            for (j in array1[i].indices) {
+                array2[i][j] = when (array1[i][j]) {
+                    '.' -> '.'
+                    '#' -> {
+                        if (countVisibleOccupied(array1, i, j, '#') >= 5) {
+                            countedChanges++
+                            'L'
+                        } else {
+                            occupiedPlaces++
+                            '#'
+                        }
+                    }
+                    else -> { // L
+                        if (countVisibleOccupied(array1, i, j, '#') <= 0) {
+                            countedChanges++
+                            occupiedPlaces++
+                            '#'
+                        } else {
+                            'L'
+                        }
+                    }
+                }
+            }
+        }
+        array1 = array2
+        array2 = Array(input.size) { i -> CharArray(input[i].size) { ' ' } }
+    } while (countedChanges != 0)
+    println(occupiedPlaces)
+}
+
+fun countVisibleOccupied(
+    array: Array<CharArray>,
+    i: Int,
+    j: Int,
+    target: Char
+) = listOf(
+    -1 to 0,
+    -1 to -1,
+    -1 to +1,
+    0 to -1,
+    0 to +1,
+    +1 to 0,
+    +1 to -1,
+    +1 to +1,
+).map { (dirX, dirY) -> toVisible(array, i, j, dirX, dirY) }
+    .count { it == target }
+
+fun toVisible(array: Array<CharArray>, i: Int, j: Int, dirX: Int, dirY: Int): Char {
+    var nexti = i + dirX
+    var nextj = j + dirY
+    while (nexti >= 0 && nexti < array.size && nextj >= 0 && nextj < array[0].size) {
+        array[nexti][nextj].let { if (it != '.') return it }
+        nexti += dirX
+        nextj += dirY
+    }
+    return '.'
+}
+
+
---
+++ src/main/java/Exercise11.kt
@@ -0,0 +1,70 @@
+fun main() {
+    val input: List<CharArray> = object {}.javaClass.getResource("input-11.txt").readText()
+        .split("\n")
+        .map(String::toCharArray)
+
+    var array1 = Array(input.size) { i ->
+        CharArray(input[i].size) { j ->
+            input[i][j]
+        }
+    }
+    var array2 = Array(input.size) { i -> CharArray(input[i].size) { ' ' } }
+    var occupiedPlaces: Int
+
+    do {
+        var countedChanges = 0
+        occupiedPlaces = 0
+        for (i in array1.indices) {
+            for (j in array1[i].indices) {
+                array2[i][j] = when (array1[i][j]) {
+                    '.' -> '.'
+                    '#' -> {
+                        if (countOccupied(array1, i, j, '#') >= 4) {
+                            countedChanges++
+                            'L'
+                        } else {
+                            occupiedPlaces++
+                            '#'
+                        }
+                    }
+                    // L
+                    else -> {
+                        if (countOccupied(array1, i, j, '#') <= 0) {
+                            countedChanges++
+                            occupiedPlaces++
+                            '#'
+                        } else {
+                            'L'
+                        }
+                    }
+                }
+            }
+        }
+        array1 = array2
+        array2 = Array(input.size) { i -> CharArray(input[i].size) { ' ' } }
+    } while (countedChanges != 0)
+    println(occupiedPlaces)
+}
+
+fun countOccupied(
+    array: Array<CharArray>,
+    i: Int,
+    j: Int,
+    target: Char
+) = listOf(
+    i - 1 to j,
+    i - 1 to j - 1,
+    i - 1 to j + 1,
+    i to j - 1,
+    i to j + 1,
+    i + 1 to j,
+    i + 1 to j - 1,
+    i + 1 to j + 1,
+).asSequence()
+    .filter { it.first >= 0 }
+    .filter { it.first < array.size }
+    .filter { it.second >= 0 }
+    .filter { it.second < array[0].size }
+    .count { array[it.first][it.second] == target }
+
+
