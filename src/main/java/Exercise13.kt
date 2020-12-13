0f72d18df09bd3748777ea78ea27af61c718601c
Nicola Corti <corti.nico@gmail.com>
2020-12-13 19:09:16 +0100

    AOC - Day 13

---
+++ src/main/java/Exercise13.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val input = object {}.javaClass.getResource("input-13.txt").readText()
+        .split("\n")
+
+    val earliestDeparture: Int = input[0].toInt()
+    val busses = input[1].split(",").filter { it != "x" }.map(String::toInt)
+
+    var time = earliestDeparture
+    while (true) {
+        val departingBus = busses.firstOrNull { time % it == 0 }
+        if (departingBus != null) {
+            println("Depart at $time with $departingBus")
+            println("${(time-earliestDeparture)*departingBus}")
+            break
+        }
+        time++
+    }
+
+}
