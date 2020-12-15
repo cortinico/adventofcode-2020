6ac063a866aa5097c49d7a834785977f0199b5a3
Nicola Corti <corti.nico@gmail.com>
2020-12-15 15:16:36 +0100

    AOC - Day 15

---
+++ src/main/java/Exercise15.kt
@@ -0,0 +1,25 @@
+fun main() {
+    val input = object {}.javaClass.getResource("input-15.txt").readText().split(",").map(String::toInt)
+
+    val map = mutableMapOf<Int, Pair<Int, Int>>()
+
+    var last = -1
+    repeat(2020) { turn ->
+        when {
+            turn < input.size -> {
+                map[input[turn]] = turn to -1
+                last = input[turn]
+            }
+            last !in map || map[last]?.second == -1 -> {
+                map[0] = turn to (map[0]?.first ?: -1)
+                last = 0
+            }
+            else -> {
+                val toSpeak = map[last]!!.first - map[last]!!.second
+                map[toSpeak] = turn to (map[toSpeak]?.first ?: -1)
+                last = toSpeak
+            }
+        }
+    }
+    println(last)
+}
