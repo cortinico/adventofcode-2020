b7a3ddee3445451175c0efc1857b6e4c5cb83c1d
Nicola Corti <corti.nico@gmail.com>
2020-12-31 15:07:03 +0100

    Setup ktfmt-gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     kotlin("jvm") version "1.4.10"
+    id("com.ncorti.ktfmt.gradle") version "0.2.0"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+ktfmt {
+    dropboxStyle()
+}
 
 repositories {
     mavenCentral()
